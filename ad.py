f807b9b16d9c54c7d2df03f43ca7416242d8b615
Maxim <info@pprog.info>
2022-11-10 22:15:54 +0500

    Refactoring and lot of new features
    Added DPI processing, making folders for fast work and more

--- ad.py
+++ ad.py
@@ -4,19 +4,17 @@ from concurrent import futures
 from tqdm import tqdm
 import time
 from datetime import datetime
-import argparse
 import os
 import sys
 import shutil
 from titlecase import titlecase
-
-# import pyperclip   # you can uncomment this if you need MacOS and Linux clipboard support
+import pyperclip
 
 def display_error(response, message):
 	print(message)
 	print(response)
 	print(response.text)
-	exit()
+	sys.exit()
 
 
 # Request from site all information about book, including titles, all metadata and such
@@ -40,7 +38,7 @@ def get_book_infos(session, url):
 		return title, links, metadata
 	else:
 		print(f"[-] Error while getting links to images of the pages!")
-		exit()  # must raise exeption, not exit!
+		sys.exit()  # must raise exeption, not exit!
 
 def format_data(content_type, fields):
 	data = ""
@@ -60,7 +58,7 @@ def login(email, password):
 	response = session.post("https://archive.org/account/login", data=data, headers=headers)
 	if "bad_login" in response.text:
 		print("[-] Wrong email or password, please check!")
-		exit()
+		sys.exit()
 	elif "Successful login" in response.text:
 		print("[+] Successfully logged in!")
 		return session
@@ -73,7 +71,7 @@ def loan(session, book_id, verbose=True):
 		"identifier": book_id
 	}
 	# 2022-07-03: This request is done by the website but we don't need to do it here.
-	# response = session.post("https://archive.org/services/loans/loan/searchInside.php", data=data)
+	response = session.post("https://archive.org/services/loans/loan/searchInside.php", data=data)
 	data['action'] = "browse_book"
 	response = session.post("https://archive.org/services/loans/loan/", data=data)
 
@@ -155,9 +153,15 @@ def download(session, n_threads, directory, links, scale, book_id):
 	images = [image_name(pages, i, directory, book_id) for i in range(len(links))]
 	return images
 
+
+# make single PDF file from multiple JPEG files
+
 def make_pdf(pdf, title, directory):
+
 	file = title+".pdf"
+
 	# Write only if file does not exist
+
 	if not os.path.isfile(os.path.join(directory, file)):
 		with open(os.path.join(directory, file),"wb") as f:
 			f.write(pdf)
@@ -165,12 +169,13 @@ def make_pdf(pdf, title, directory):
 	else:
 		print(f"[-] PDF file \"{file}\" already present on disk")
 
-if __name__ == "__main__":
+# set all config values
 
-	print("Archive Downloader 2022.11.1")
+def process_config(config_file_name):
 
-	if len(sys.argv) == 1:
-		print("Note that you can specify configuration file in parameters like AD C:\Path\To\MyConfig.txt")
+	global email, password, scale, n_threads, d, outtype, urls, editingprep
+
+	file1 = open(config_file_name, 'r')     
 
 	# set default values
 
@@ -181,20 +186,10 @@ if __name__ == "__main__":
 	d = os.getcwd()	
 	outtype = "jpg"
 	urls = []
-	myfile = 'ad.txt'
+	
+	editingprep = 'none'
 
-	# use custom configuation file if supplied
-
-	if len(sys.argv) == 2:
-		myfile = sys.argv[1]
-
-	if not os.path.isfile(myfile):
-		print("Can't find configuration file, exiting!")
-		exit()
-
-	file1 = open(myfile, 'r')     
-
-        # Set current parsing mode to none
+	# Set current parsing mode to none
 
 	mode_pars = "none"
   
@@ -221,6 +216,8 @@ if __name__ == "__main__":
 				mode_pars = "outtype"
 			if line.find("# Folder and file naming")==0:
 				mode_pars = "naming"
+			if line.find("# Preparation for editing")==0:
+				mode_pars = "prep"	
 
 			if line.find("# Book URLs")==0:
 				mode_pars = "urls"
@@ -231,6 +228,8 @@ if __name__ == "__main__":
 			password = line	
 		if  mode_pars ==  "outdir":
 			d = line	
+		if  mode_pars ==  "prep":
+			editingprep = line		
 		if  mode_pars ==  "resolution":
 			scale = int(line)
 		if  mode_pars ==  "threads":
@@ -245,31 +244,145 @@ if __name__ == "__main__":
   
 	file1.close()
 
-	if not os.path.isdir(d):
-		print(f"Output directory does not exist!")
-		exit()
-
-	# Universal clipboard support, you can comment this if you need MacOS and Linux clipboard support
-
+def get_clipboard_content():
 	clipboard_cont = ""
 
-	# clipboard_cont = pyperclip.paste() # you can uncomment this if you need MacOS and Linux clipboard support
-	# pyperclip.copy('')  # such way we won't reuse old clipboard contents next time, you can uncomment this if you need MacOS and Linux clipboard support
+	clipboard_cont = pyperclip.paste() # Cross platform clipboard support
+	pyperclip.copy('')  # such way we won't reuse old clipboard contents next time, you can uncomment this if you need MacOS and Linux clipboard support	
 
-	# Windows specific part, you can comment this if you need MacOS and Linux clipboard support
-	if sys.platform == "win32":
-		import win32clipboard
-		win32clipboard.OpenClipboard()
+	return clipboard_cont.splitlines()
+
+def make_pdf_metadata(metadata):
+	# prepare PDF metadata
+    # keywords are in 'subject'  
+    # ISBN can be got from isbn': ['9780981803982', '0981803989']
+    # 'creator': 'Kingsley, Eve', 'date': '2008'
+	# sometimes archive metadata is missing
+	pdfmeta = { }
+	# ensure metadata are str
+	for key in ["title", "creator", "associated-names"]:
+		if key in metadata:
+			if isinstance(metadata[key], str):
+				pass
+			elif isinstance(metadata[key], list):
+				metadata[key] = "; ".join(metadata[key])
+			else:
+				raise Exception("unsupported metadata type")
+	# title
+	if 'title' in metadata:
+		pdfmeta['title'] = titlecase(metadata['title'])
+
+	# author, we have issue here as we need sometimes to modify names from Rayan, Jack to Jack Rayan
+
+	authors_list = ""
+
+	if 'creator' in metadata:
+		authors_list = metadata['creator']
+	if 'associated-names' in metadata:
+		if not authors_list == "":
+			authors_list = authors_list  + ";"
+		authors_list = authors_list + metadata['associated-names']
+
+	authors_split = authors_list.split(";")
+	authors_list = ""
+
+	for author in authors_split:
+		author_res=""
+		for ch in author:
+			if ch not in ['0','1','2','3','4','5','6','7','8','9','-']:
+				author_res+=ch
+		if not author_res.find(",")==-1:
+			author_split = author_res.split(",")
+			author_res = author_split[1].strip()+" "+author_split[0].strip()
+		if authors_list=="":
+			authors_list = author_res
+		else:
+			authors_list = authors_list + " & " + author_res
+
+	pdfmeta['author'] = authors_list
+
+	if 'date' in metadata:
 		try:
-			if not win32clipboard.IsClipboardFormatAvailable(win32clipboard.CF_UNICODETEXT)==0:
-				clipboard_cont = win32clipboard.GetClipboardData(win32clipboard.CF_UNICODETEXT)
-			win32clipboard.EmptyClipboard()
-		finally:
-			win32clipboard.CloseClipboard()
+			pdfmeta['creationdate'] = datetime.strptime("1 June " + metadata['date'], '%d %B %Y')
+			pdfmeta['moddate'] = pdfmeta['creationdate']
+		except:
+			pass
+	# keywords
 
-	# end of Windows specific part, you can comment this if you need MacOS and Linux clipboard support
+	pdfmeta['keywords'] = [f"https://archive.org/details/{book_id}"]
 
-	clipboard_list = clipboard_cont.splitlines()
+	if 'subject' in metadata:
+		if isinstance(metadata['subject'], list):
+			pdfmeta['keywords'] =  pdfmeta['keywords'] + metadata['subject']
+		else:
+			pdfmeta['keywords'] =  pdfmeta['keywords'] + [metadata['subject']]
+
+	if 'isbn' in metadata:
+		if isinstance(metadata['isbn'], list):
+			pdfmeta['keywords'] =  pdfmeta['keywords'] + metadata['isbn']
+		else:
+			pdfmeta['keywords'] =  pdfmeta['keywords'] + [metadata['isbn']]
+		
+
+	if 'date' in metadata:
+		if isinstance(metadata['date'], list):
+			pdfmeta['keywords'] =  pdfmeta['keywords'] + metadata['date']
+		else:
+			pdfmeta['keywords'] =  pdfmeta['keywords'] + [metadata['date']]
+
+	return pdfmeta
+
+
+# function to patch DPI values in all jpeg files
+
+def Patch_DPI(images):
+	print('Changing DPI settings for all images to 300dpi')
+
+	res_bytes = (300).to_bytes(2, byteorder='big')
+	inches_value = bytearray([1])
+	total_patch = inches_value + res_bytes + res_bytes
+
+	with tqdm(total=len(images)) as t:
+		for myimage in images:		
+			if os.path.isfile(myimage):
+				with open(myimage,'r+b') as f:
+					f.seek(6) # jump to JFIF
+					jfif_signature = f.read(4)				
+					if jfif_signature == b'JFIF':														
+						f.seek(13) # jump to resolution values
+						if f.read(len(total_patch )) != total_patch:
+							f.seek(13) # jump to resolution values
+							f.write(total_patch)
+			t.update(1)				
+	
+
+# start of main body
+
+if __name__ == "__main__":
+
+	print("Archive Downloader 2022.11.3")
+
+	if len(sys.argv) == 1:
+		print("Note that you can specify configuration file in parameters like AD C:\Path\To\MyConfig.txt")	
+
+	myfile = 'ad.txt'	
+
+	# use custom configuation file if supplied
+
+	if len(sys.argv) == 2:
+		myfile = sys.argv[1]
+
+	if not os.path.isfile(myfile):
+		print("Can't find configuration file, exiting!")
+		sys.exit()
+
+	process_config(myfile)
+
+	if not os.path.isdir(d):
+		print(f"Output directory does not exist!")
+		sys.exit()
+
+	clipboard_list = get_clipboard_content()
 
 	for clip_url in clipboard_list:
 		if clip_url.startswith("https://archive.org/details/") and not clip_url in urls:
@@ -287,10 +400,9 @@ if __name__ == "__main__":
 	
 	if len(books)==0:
 		print("No correct books URLs to download, exiting!")
-		exit()
+		sys.exit()
 	else:	
 		print(f"{len(books)} Book(s) will be downloaded") 
-
 	
 
 	session = login(email, password)
@@ -303,106 +415,72 @@ if __name__ == "__main__":
 		session = loan(session, book_id)
 		title, links, metadata = get_book_infos(session, url)
 
-		directory = os.path.join(d, title)
+		directory = os.path.join(d, title) # "\\\\?\\" + os.path.join(d, title)
 
 		if not os.path.isdir(directory):
 			os.makedirs(directory)
+
 		if 'title' in metadata:
 			print("Current book title: "+ titlecase(metadata['title']))                
 
 		images = download(session, n_threads, directory, links, scale, book_id)
 
+		if editingprep != "none":				
+			Patch_DPI(images)
+
+			# make all subdirectories only if in this edit preparation mode		
+
+			if not os.path.isdir(os.path.join(directory, "Meta")):
+				os.makedirs(os.path.join(directory, "Meta"))
+			if not os.path.isdir(os.path.join(directory, "Cover")):
+				os.makedirs(os.path.join(directory, "Cover"))	
+			if not os.path.isdir(os.path.join(directory, os.path.join("Cover","Proc"))):
+				os.makedirs(os.path.join(directory, os.path.join("Cover","Proc")))		
+			if not os.path.isdir(os.path.join(directory, "Empty")):
+				os.makedirs(os.path.join(directory, "Empty"))		
+			if not os.path.isdir(os.path.join(directory, "Proc")):
+				os.makedirs(os.path.join(directory, "Proc"))
+			if not os.path.isdir(os.path.join(directory, "Proj")):
+				os.makedirs(os.path.join(directory, "Proj"))	
+			if not os.path.isdir(os.path.join(directory, "Illust")):
+				os.makedirs(os.path.join(directory, "Illust"))			
+
+
 		if outtype in ("pdf","jpgpdf","jpgpdfmeta","jpgepub"): # any modes that require creation of PDF or EPUB file
 			import img2pdf
+			
+			pdfmeta = make_pdf_metadata(metadata)			
 
-			# prepare PDF metadata
-                        # keywords are in 'subject'  
-                        # ISBN can be got from isbn': ['9780981803982', '0981803989']
-                        # 'creator': 'Kingsley, Eve', 'date': '2008'
-			# sometimes archive metadata is missing
-			pdfmeta = { }
-			# ensure metadata are str
-			for key in ["title", "creator", "associated-names"]:
-				if key in metadata:
-					if isinstance(metadata[key], str):
-						pass
-					elif isinstance(metadata[key], list):
-						metadata[key] = "; ".join(metadata[key])
-					else:
-						raise Exception("unsupported metadata type")
-			# title
-			if 'title' in metadata:
-				pdfmeta['title'] = titlecase(metadata['title'])
+			if outtype=="jpgpdfmeta":											
+				pdf = img2pdf.convert(images[0], **pdfmeta)		
 
-			# author, we have issue here as we need sometimes to modify names from Rayan, Jack to Jack Rayan
+				# In preparation mode we place meta PDF in special folder and move first and last image into cover folder
 
-			authors_list = ""
-
-			if 'creator' in metadata:
-				authors_list = metadata['creator']
-			if 'associated-names' in metadata:
-				if not authors_list == "":
-					authors_list = authors_list  + ";"
-				authors_list = authors_list + metadata['associated-names']
-
-			authors_split = authors_list.split(";")
-			authors_list = ""
-
-			for author in authors_split:
-				author_res=""
-				for ch in author:
-					if ch not in ['0','1','2','3','4','5','6','7','8','9','-']:
-						author_res+=ch
-				if not author_res.find(",")==-1:
-					author_split = author_res.split(",")
-					author_res = author_split[1].strip()+" "+author_split[0].strip()
-				if authors_list=="":
-					authors_list = author_res
+				if editingprep != "none":
+					make_pdf(pdf, title[:25], os.path.join(directory, "Meta")) # shorten title in case of meta to avoid too long paths						
 				else:
-					authors_list = authors_list + " & " + author_res
+					make_pdf(pdf, title, d)
+			else:
+				pdf = img2pdf.convert(images, **pdfmeta)
+				make_pdf(pdf, title, d)			
 
-			pdfmeta['author'] = authors_list
+		# move first and last images into separate Covers folder
 
-			# print(metadata)
+		if editingprep != "none":	
+			image1 = os.path.split(images[0]) # first one				
+			image2 = os.path.split(images[-1]) # last one	
+			image1_path = os.path.join(os.path.join(image1[0],"Cover"), image1[1])
+			image2_path = os.path.join(os.path.join(image2[0],"Cover"), image2[1])
+			if not os.path.exists(image1_path):
+				os.rename(images[0], image1_path)
+			else:
+				os.remove(images[0])	
+			if not os.path.exists(image2_path):
+				os.rename(images[-1], image2_path)
+			else:
+				os.remove(images[-1])
 
-			if 'date' in metadata:
-				try:
-					pdfmeta['creationdate'] = datetime.strptime("1 June " + metadata['date'], '%d %B %Y')
-					pdfmeta['moddate'] = pdfmeta['creationdate']
-				except:
-					pass
-			# keywords
-
-			pdfmeta['keywords'] = [f"https://archive.org/details/{book_id}"]
-
-			if 'subject' in metadata:
-				if isinstance(metadata['subject'], list):
-					pdfmeta['keywords'] =  pdfmeta['keywords'] + metadata['subject']
-				else:
-					pdfmeta['keywords'] =  pdfmeta['keywords'] + [metadata['subject']]
-
-			if 'isbn' in metadata:
-				if isinstance(metadata['isbn'], list):
-					pdfmeta['keywords'] =  pdfmeta['keywords'] + metadata['isbn']
-				else:
-					pdfmeta['keywords'] =  pdfmeta['keywords'] + [metadata['isbn']]
-				
-
-			if 'date' in metadata:
-				if isinstance(metadata['date'], list):
-					pdfmeta['keywords'] =  pdfmeta['keywords'] + metadata['date']
-				else:
-					pdfmeta['keywords'] =  pdfmeta['keywords'] + [metadata['date']]
-
-			# print(metadata['subject'])
-
-			if outtype=="jpgpdfmeta":				
-				images = images[0]
-
-			pdf = img2pdf.convert(images, **pdfmeta)
-
-			make_pdf(pdf, title, d)
-			if outtype=="pdf":
+		if outtype=="pdf":
 				try:
 					shutil.rmtree(directory)
 				except OSError as e:
